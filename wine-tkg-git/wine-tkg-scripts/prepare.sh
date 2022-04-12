b7b110101c4305e255b692d25d9f605bd344efef
vamman <vamman@gmail.com>
2022-04-11 21:45:58 -0300

    Update prepare.sh

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -1546,12 +1546,12 @@ EOM
 	fi
 
 	# _nvidia pixelos
-	if ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor 05d5df3292d6ae3ad5b2fa540bedbc011285c4d3 HEAD ); then
+	if ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor 0c249e6125fc9dc6ee86b4ef6ae0d9fa2fc6291b HEAD ); then
 		_patchname='0001-nvencodeapi-First-implementation.patch' && _patchmsg="Applied NVEnc patch 1" && nonuser_patcher
 		echo "PixelOS patched NVEnc 1" >> "$_where"/last_build_config.log
 	fi
 
-	if ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor 279a1c527003811333646bcb827fda38618d8f21 HEAD ); then
+	if ( cd "${srcdir}"/"${_stgsrcdir}" && git merge-base --is-ancestor 0c249e6125fc9dc6ee86b4ef6ae0d9fa2fc6291b HEAD ); then
 		_patchname='0003-nvencodeapi-Add-support-for-version-6.0.patch' && _patchmsg="Applied NVEnc patch 2" && nonuser_patcher
 		echo "PixelOS patched NVEnc 2" >> "$_where"/last_build_config.log
 	fi
